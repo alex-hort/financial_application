cd19219042542e7f9f07baf1b4213fe17fb744e3
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-06-19 19:38:38 -0600

    feat 3: create search result model

---
+++ FinacialApp/Service/APIService.swift
@@ -0,0 +1,14 @@
+//
+//  APIService.swift
+//  FinacialApp
+//
+//  Created by Alexis Horteales Espinosa on 19/06/25.
+//
+
+
+import Foundation
+
+
+struct APIService{
+    let API_KEY = "T4OXHJZG46PU6GFZ"
+}
