0dd571017e16738af306c85a874471aab802e79c
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-06-19 19:22:18 -0600

    feat 2: add @IBOutlet weak var and associate them with the main storyboard.

---
+++ FinacialApp/View/SearchTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTableViewCell.swift
+//  FinacialApp
+//
+//  Created by Alexis Horteales Espinosa on 19/06/25.
+//
+
+import UIKit
+
+
+class SearchTableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var assetNameLabel: UILabel!
+    @IBOutlet weak var assetSymbolLabel: UILabel!
+    @IBOutlet weak var assetTyeeLabel: UILabel!
+    
+}
